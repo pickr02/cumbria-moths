8c81b34d4e4efcfd2abb98f76f2d6b9e5b29dd2e
Rich Burkmar <rich.burkmar@gmail.com>
2023-11-09 12:12:19 +0000

    Scoping

--- core/js/test.js
+++ core/js/test.js
@@ -13,14 +13,4 @@ $(document).ready(function() {
     $("#atlas-site-name").text(`No site name specified` )
   })
 
-  // // Test adding a drop-down list
-  // const $div = $('<div class="dropdown">')
-  // $div.appendTo($("#atlas-site-content"))
-  // const $button = $('<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">')
-  // $button.html('Dropdown Example&nbsp;<span class="caret"></span>')
-  // $button.appendTo($div)
-  // const $ul = $('<ul class="dropdown-menu">').appendTo($div)
-  // $('<li><a href="#">HTML</a></li>').appendTo($ul)
-  // $('<li><a href="#">CSS</a></li>').appendTo($ul)
-
 })
