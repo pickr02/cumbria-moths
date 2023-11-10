21afb5ee4cecc1bff22106b3b7e5d8b9bc0c2afa
Rich Burkmar <rich.burkmar@gmail.com>
2023-11-10 17:43:19 +0000

    More relative path test

--- core/js/test.js
+++ core/js/test.js
@@ -16,7 +16,8 @@ $(document).ready(function() {
     if (location.hostname !== "localhost" && location.hostname !== "127.0.0.1") {
       repo = `/${json.repo}`
     }
-    d3.csv(`${repo}/user/data/taxa.csv`).then(data => {
+    //d3.csv(`${repo}/user/data/taxa.csv`).then(data => {
+    d3.csv(`user/data/taxa.csv`).then(data => {
       data.forEach(d => {
         const $opt = $('<option>').appendTo($('#atlas-taxa-select'))
         $opt.text(d.taxon)
