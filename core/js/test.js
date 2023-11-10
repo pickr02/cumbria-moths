b03ee817ab7700c0606d1f61335d1065fff5f3f9
Rich Burkmar <rich.burkmar@gmail.com>
2023-11-10 16:41:46 +0000

    Added basic mapping test

--- core/js/test.js
+++ core/js/test.js
@@ -1,3 +1,5 @@
+let mapStatic
+
 $(document).ready(function() {
 
   // Open test config file if it exists
@@ -13,4 +15,54 @@ $(document).ready(function() {
     $("#atlas-site-name").text(`No site name specified` )
   })
 
+  // Populate taxon drop-down
+  d3.csv("../../user/data/taxa.csv").then(data => {
+    data.forEach(d => {
+      const $opt = $('<option>').appendTo($('#atlas-taxa-select'))
+      $opt.text(d.taxon)
+      $opt.attr('value', d.taxonId)
+    })
+  })
+
+  // Initialise map
+  mapStatic = brcatlas.svgMap({
+    selector: "#atlas-static-map",
+    mapTypesKey: 'Standard hectad',
+    seaFill: 'white',
+    //expand: true,
+    transOptsKey: 'BI4',
+    mapTypesControl: false,
+    transOptsControl: false,
+    mapTypesSel: {hectad: genHecatdMap},
+    mapTypesKey: 'hectad'
+  })
 })
+
+function atlasTaxonSelected() {
+  const taxonId = $('#atlas-taxa-select').find(":selected").val()
+  console.log('selected', taxonId)
+
+  mapStatic.setIdentfier(`../../user/data/hectad/${taxonId}.csv`)
+  mapStatic.redrawMap()
+}
+
+async function genHecatdMap(file) {
+
+  const data = await d3.csv(file)
+
+  const dataMap = data.map(d => {
+    return {gr: d.gr, colour: 'black'}
+  })
+
+  console.log('dataMap', dataMap)
+
+  return new Promise((resolve) => {
+    resolve({
+      records: dataMap,
+      precision: 10000,
+      shape: 'circle',
+      opacity: 1,
+      size: 1
+    })
+  })
+}
