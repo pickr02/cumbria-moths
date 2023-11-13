689f356fbbd8329eeb79697a3a0f0168b1d0dad3
Rich Burkmar <rich.burkmar@gmail.com>
2023-11-13 16:56:49 +0000

    Restructured reading of config. Added more config.

--- core/js/components.js
+++ core/js/components.js
@@ -1,21 +1,70 @@
 $(document).ready(function() {
 
+ (async function() {
+    const configSite = await getConfig("../user/config/site.json") 
+    const configCore = await getConfig("config/config.json") 
+    
+    //console.log('configSite', configSite)
+    //console.log('configCore', configCore)
+
+    generateHeader(configSite)
+    generateFooter(configCore)
+  })()
+})
+
+function getConfig(file) {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: file,
+      cache: false,
+      // async:  false,
+      success: function (data) {
+        resolve(data)
+      },
+      error: function (error) {
+        resolve({})
+      }
+    })
+  })
+}
+
+function generateFooter(configCore) {
   // Footer
   const $footer = $("#brc-atlas-footer")
   $footer.attr("class", "pt-5 my-5 text-body-secondary border-top")
   $footer.html(`
-    Created using <a href="https://github.com/BiologicalRecordsCentre/brevi-atlas-test">
-    BRC configurable atlas project</a> (version <b><span id="brc-atlas-footer-version"></span></b>.)
-    <img class="pt-2" src="images/BRC_UKCEH_logo_small.png"></img>
+    <span class="me-2">
+      Created using <a href="https://github.com/BiologicalRecordsCentre/brevi-atlas-test">
+      BRC configurable atlas project</a> (version <b>${configCore.version}</b>).
+    </span>
+    <a href="admin.html">
+      <img class="pt-2 pb-2" style="width:30px" src="images/gear-icon.png"></img>
+    </a>
+    <div>
+      <img class="pt-2 pb-2" src="images/BRC_UKCEH_logo_small.png"></img>
+    </div>
   `)
+}
 
-  // Set version in footer
-  $.ajax({
-    url: "config/config.json",
-    cache: false
-  })
-  .done(function( json ) {
-    console.log(json)
-    $("#brc-atlas-footer-version").text(`${json.version}` )
-  })
-})
+function generateHeader(configSite) {
+
+  const name = configSite.name ? configSite.name : "Site name configuration not specied"
+  const headerLogo = configSite['header-logo']
+  const logoHeight = configSite['header-logo-height']
+  // Header
+  const $header = $("#brc-atlas-header")
+  $header.attr("class", "d-flex align-items-center pb-3 mb-5 border-bottom fs-2")
+
+  // Site logo
+  console.log(headerLogo)
+  if (headerLogo) {
+    const $img = $(`<img src="../user/config/${headerLogo}">`).appendTo($header)
+    if (logoHeight) {
+      $img.css("height", `${logoHeight}px`)
+      $img.css("margin-right", "10px")
+    }
+  }
+
+  // Site name
+  $('<span>').text(name).appendTo($header)
+}
