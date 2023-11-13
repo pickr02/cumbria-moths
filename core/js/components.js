3d738dafd9af62c1c4df5893db36c2da2b4b5570
Rich Burkmar <rich.burkmar@gmail.com>
2023-11-13 13:04:07 +0000

    Developed footer component

---
+++ core/js/components.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+  // Footer
+  const $footer = $("#brc-atlas-footer")
+  $footer.attr("class", "pt-5 my-5 text-body-secondary border-top")
+  $footer.html(`
+    Created using <a href="https://github.com/BiologicalRecordsCentre/brevi-atlas-test">
+    BRC configurable atlas project</a> (version <b><span id="brc-atlas-footer-version"></span></b>.)
+    <img class="pt-2" src="/core/images/BRC_UKCEH_logo_small.png"></img>
+  `)
+
+  // Set version in footer
+  $.ajax({
+    url: "/core/config/config.json",
+    cache: false
+  })
+  .done(function( json ) {
+    console.log(json)
+    $("#brc-atlas-footer-version").text(`${json.version}` )
+  })
+})
